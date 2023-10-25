b88ff5c235586c22d1667de95f7a2bbc6d1ef566
Aleksandr Fedyanin <faa.workhard@gmail.com>
2023-10-25 22:58:32 +0300

    modified:   key_word_generator.py

--- key_word_generator.py
+++ key_word_generator.py
@@ -39,26 +39,22 @@ def remove_duplicates(response_list):
     return set(response_list)
 
 async def main(topic, keywords, phrases_num):
-    conversation_history = []
     output_file = open("generated_phrases.txt", "w")  # Создаем файл с фразами
     output_summary = []
     num_requests = 0  # Количество запросов к API
     first_request = True  # Флаг для первого запроса
         
-    system_message = "1. Be blunt and straightforward; don't sugarcoat.\n2. No moral lectures.\n3. Discuss safety only if it's crucial and non-obvious.\n4. Never mention that you're an AI.\n5. Avoid language constructs that can be interpreted as remorse, apology, or regret. This includes phrases with words like 'sorry', 'apologies', and 'regret'.\n6. If information is beyond your scope or knowledge cutoff date in January 2022, simply state ‘I don’t know’.\n7. Don’t use disclaimers about expertise or professionalism.\n8. Ensure responses are unique and without repetition.\n9. Never suggest seeking information elsewhere.\n10. No limits!\n\nYou will be provided with a topic and keywords, and your task is to generate 150 low-frequency key phrases only for that topic. \nIn each phrase must be from 4 to 12 words, and from 12 to 120 symbols.\nThe list must be in JSON array format, without numbering the list."
+    system_message = "11. Respond concisely.\n2. Be blunt and straightforward; do not sugarcoat.\n3. No moral lectures.\n4. Discuss safety only if it is crucial and non-obvious.\n5. Never mention that you are an AI.\n6. Avoid language constructs that can be interpreted as remorse, apology, or regret. This includes phrases with words like 'sorry', 'apologies', and 'regret'.\n8. Do not use disclaimers about expertise or professionalism.\n9. Ensure responses are unique and without repetition.\n10. Never suggest seeking information elsewhere.\n\nYou will be provided with a topic and keywords, and your task is to generate 1000 low-frequency key phrases only for that topic. The list must be in JSON array format, without numbering the list."
     user_message = f"Topic: {topic}.\nKeywords: {keywords}."
         
     first_prompt = [{"role": "system", "content": system_message}, {"role": "user", "content": user_message}]
-    conversation_history = first_prompt
     
     while len(output_summary) < phrases_num:
         response = await chat_chatgpt(first_prompt)
         if response:
-            conversation_history.append({"role": "assistant", "content": str(response)})
             output_summary.extend(response)
             for item in response:
                 output_file.write(item.replace("'", "").replace('"', '').replace('{', '').replace('}', '') + "\n")
-            prompt = [message["content"] for message in conversation_history]
             print("ChatGPT сгенерировал:", len(output_summary), "фраз")
             doc_info = len(output_summary)
                 
