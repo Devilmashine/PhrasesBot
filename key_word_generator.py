cdff07ae16bbfbaca849ca90bc874f09463f03c5
Aleksandr Fedyanin <faa.workhard@gmail.com>
2023-10-27 22:04:20 +0300

    modified:   handlers.py 	modified:   key_word_generator.py 	modified:   states.py

--- key_word_generator.py
+++ key_word_generator.py
@@ -6,12 +6,9 @@ import json
 import config
 
 openai.api_key = config.OPENAI_TOKEN
-global stop_execution
-stop_execution = False
 
-async def chat_chatgpt(prompt) -> list[str]:
+async def main(prompt) -> list[str]:
     """Generates a list of phrases from a given prompt using the ChatGPT language model."""
-
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
@@ -51,44 +48,6 @@ def remove_duplicates(response_list):
 
     return set(response_list)
 
-async def main(topic: str, keywords: str, phrases_num: int) -> str:
-    """Generates a list of phrases for a given topic and keywords using the ChatGPT language model.
-
-    Args:
-        topic: The topic for the phrases.
-        keywords: The keywords to use in the phrases.
-        phrases_num: The number of phrases to generate.
-
-    Returns:
-        The path to the file containing the generated phrases.
-    """
-
-    output_file = open("generated_phrases.txt", "w")  # Create a file to store the generated phrases
-    output_summary = []
-    system_message = "1. Respond concisely.\n2. Be blunt and straightforward; do not sugarcoat.\n3. No moral lectures.\n4. Discuss safety only if it is crucial and non-obvious.\n5. Never mention that you are an AI.\n6. Avoid language constructs that can be interpreted as remorse, apology, or regret. This includes phrases with words like 'sorry', 'apologies', and 'regret'.\n8. Do not use disclaimers about expertise or professionalism.\n9. Ensure responses are unique and without repetition.\n10. Never suggest seeking information elsewhere.\n\nYou will be provided with a topic and keywords, and your task is to generate 1000 low-frequency key phrases only for that topic. The list must be in JSON array format, without numbering the list."
-    user_message = f"Topic: {topic}.\nKeywords: {keywords}."
-    first_prompt = [{"role": "system", "content": system_message}, {"role": "user", "content": user_message}]
-
-    while len(output_summary) < phrases_num:
-        if stop_execution:
-            break
-
-        start_time = time.monotonic()
-
-        response = await chat_chatgpt(first_prompt)
-        if response:
-            output_summary.extend(response)
-            for item in response:
-                output_file.write(item.replace("'", "").replace('"', '').replace('{', '').replace('}', '') + "\n")
-            logging.info("ChatGPT generated: {} phrases".format(len(output_summary)))
-   
-        end_time = time.monotonic()
-        iteration_time = end_time - start_time
-        if iteration_time < 20:
-            await asyncio.sleep(iteration_time)  # Пауза между запросами
-
-    output_file.close()
-    return
 
 # Run the main function
 if __name__ == "__main__":
