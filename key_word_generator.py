15c41c1f1b8a3b4d4e5eb08ba2bba9a4305487dd
Aleksandr Fedyanin <workhard77@mail.ru>
2023-11-06 00:57:24 +0300

    modified:   handlers.py 	modified:   key_word_generator.py

--- key_word_generator.py
+++ key_word_generator.py
@@ -1,49 +1,40 @@
 import asyncio
-import os
-from dotenv import load_dotenv
 import time
 import openai
 import logging
 import json
 from collections import OrderedDict
 
-load_dotenv(dotenv_path=".env")
-
-OPENAI_TOKENS = os.getenv("OPENAI_TOKENS")
-keys = OPENAI_TOKENS.split(",")
-
-async def generate_phrases(prompt: str, api_key: str) -> set[str]:
+async def main(prompt: str, api_key: str) -> set[str]:
     """Generates a list of phrases from a given prompt using the ChatGPT language model."""
     try:
         start_time = time.time()
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=prompt,
-            max_tokens=7000,
-            top_p=1,
-            temperature=1,
-            frequency_penalty=0.1,
-            presence_penalty=0.1,
-            api_key=api_key
-        )  # Use the current API key
-        final_response = response.choices[0].message["content"]  # type: ignore
-        response_list = parse_final_response(final_response)
+        response = await generate_response(prompt, api_key)
+        response_list = parse_final_response(response)
         response_list = remove_duplicates(response_list)
         end_time = time.time()
         iteration_time = end_time - start_time
         if iteration_time < 20:
-            await asyncio.sleep(iteration_time)  # Pause between requests
+            await asyncio.sleep(20 - iteration_time)  # Pause between requests
         return response_list
     except Exception as e:
         logging.error(f"Error occurred: {e}")
         return []
 
-async def main(prompt):
-    """Runs the main function in parallel using multiple API keys."""
-    tasks = [generate_phrases(prompt, api_key) for api_key in keys]
-    results = await asyncio.gather(*tasks)
-    generated_phrases = [result for sublist in results for result in sublist]
-    return generated_phrases
+async def generate_response(prompt: str, api_key: str) -> str:
+    """Generates a response using the ChatGPT language model."""
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=prompt,
+        max_tokens=7000,
+        top_p=1,
+        temperature=1,
+        frequency_penalty=0.1,
+        presence_penalty=0.1,
+        api_key=api_key
+    )  # Use the current API key
+    final_response = response.choices[0].message["content"]  # type: ignore
+    return final_response
 
 def parse_final_response(final_response: str) -> list[str]:
     """Parses the final response from the ChatGPT language model into a list of phrases."""
