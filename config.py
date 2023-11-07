4383b39d8e61edc58b6b26cf851fd6940ab9b52f
Aleksandr Fedyanin <faa.workhard@gmail.com>
2023-11-08 00:37:15 +0300

    modified:   config.py 	modified:   handlers.py 	modified:   key_word_generator.py 	new file:   openai-test.py 	modified:   requirements.txt

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
 class Settings(BaseSettings):
-    bot_token: SecretStr
-    sqlalchemy_url: SecretStr
-    openai_tokens: str
+    bot_token: SecretStr = SecretStr(os.getenv("BOT_TOKEN"))
+    sqlalchemy_url: SecretStr = SecretStr(os.getenv("SQLALCHEMY_URL"))
+    openai_tokens: str = os.getenv("OPENAI_TOKENS")
 
 
 config = Settings()
